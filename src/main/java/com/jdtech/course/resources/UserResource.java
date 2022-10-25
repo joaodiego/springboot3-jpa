438ac46fb029e9c5176dbdefea2c88c37222ee0a
joaodiego <joaodiegor@hotmail.com>
2022-10-25 10:53:06 -0300

    User entity and resource

---
+++ src/main/java/com/jdtech/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.jdtech.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jdtech.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Diego", "diego@gmail.com", "333-3333", "senha");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
